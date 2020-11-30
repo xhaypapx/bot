eb82e7777861d55abf2480cddee81f8be72f96cb
xhaypapx <68760383+xhaypapx@users.noreply.github.com>
2020-11-30 22:09:01 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,10 @@
+{
+  "prefix": "b!",
+  "sahip": "534094925541670913",
+  "token": "Botun Tokeni",
+  "otorol": "662956370357649408",
+  "yetkili": "534094925541670913",
+  "hgbbkanal": "662395200404062283",
+  "güvenlik" : "662395276060917761",
+  "sunucuadı" : "AŞİRET - İ BÂÐ TEAM"
+}
